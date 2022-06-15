bd25d920776af7e697aad048df24dbfa6366df53
tranduc01 <doantranduc01@gmail.com>
2022-06-15 11:55:00 +0700

    refactor the main code and green 4ever always

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 ### You will find in this repo the following stuff
 * The Math-Util library that offers multiple math functions ( a Java-based source code)
 * Unit Testing source code with JUnit Framework
+* GitHub Actions to demonstrate the Continuous Integration (CI) technique
+* Regression Test
 * ...
 
 ### Screenshots 
 ![DDT & TDD with JUnit](https://github.com/tranduc01/math-util/blob/main/image/DDT%20with%20JUnit.png)
 
+! <img>
+[alt=""]
+(src="")
 
 #### © 2022 tranduc01
--- src/com/tranduc/mathutil/core/MathUtil.java
+++ src/com/tranduc/mathutil/core/MathUtil.java
@@ -14,7 +14,7 @@ public class MathUtil {
     // clone class Math cua JDK
     // ham thu vien xai chung cho ai do ma ko can luu lai cac trang thai/gia tri
     //chon thiet ke la ham static
-    
+
     // ham tinh giai thua !!!
     //n!=1.2.3.....n
     // ko co giai thua cho so am
@@ -23,19 +23,42 @@ public class MathUtil {
     //20 giai thua 18 con so 0, vua kip du cho kieu long cua Java
     //21 giai thua tran kieu long
     //bai nay quy uoc tinh n! trong khoang 0-20
-    public static long getFactorial(int n){
+//    public static long getFactorial(int n){
+//        if(n<0 || n>20){
+//            throw new IllegalArgumentException("Invalid Argument. N must be between 0 and 20");        
+//        }
+//        if(n==0 || n==1){
+//            return 1; //ket thuc cuoc choi som neu nhan nhug dau vao dac biet
+//        }
+//        long product=1; //tich nhan don
+//        for (int i = 2; i <= n; i++) 
+//            product *= i; //product=product*i;
+//           
+//        return product;
+//        
+//    }
+    //Hoc de quy torng vong 30s
+    //Hien tuong goi lai chinh minh voi 1 quy mo khac
+    //vd: con bup be cua Nga, giong nhau va long trong nhau
+    //tinh dum tui 6!
+    //n! = 1.2.3.4.5......n
+    //6! = 6.5!
+    //5! = 5.4!
+    //4! = 4.3!
+    //3! = 3.2!
+    //2! = 2.1!  //diem dung
+    //1 != 1
+    //quy uoc: 1! = 0! = 1
+    //chot ha: n! = n*(n-1)!
+    public static long getFactorial(int n) {
+
         if(n<0 || n>20){
-            throw new IllegalArgumentException("Invalid Argument. N must be between 0 and 20");        
+            throw new IllegalArgumentException("n must be between 0...20. Plz!");
         }
-        if(n==0 || n==1){
+        if (n == 0 || n == 1) {
             return 1; //ket thuc cuoc choi som neu nhan nhug dau vao dac biet
         }
-        long product=1; //tich nhan don
-        for (int i = 2; i <= n; i++) 
-            product *= i; //product=product*i;
-           
-        return product;
-        
+        return n * getFactorial(n - 1);
+
     }
-    
 }
